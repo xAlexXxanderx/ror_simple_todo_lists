6b000b42281cb3b47519abfd653f0ca7a1a43c24
Alexander Grechko <alexxxander1111@gmail.com>
2015-03-31 02:13:13 +0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RorSimpleTodoLists::Application.config.secret_key_base = '399e9ffe2937cd54101191908b9714e8c91cff6c2038dfa91139e92a402e98b3aff06a8df856efe242350274d1475f7d5acf9b5bf1501ef7ad92d9ada9018549'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RorSimpleTodoLists::Application.config.session_store :cookie_store, key: '_ror_simple_todo_lists_session'
